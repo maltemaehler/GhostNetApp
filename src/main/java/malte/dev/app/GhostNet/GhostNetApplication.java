7dd8cd070ebab2c036437f6c64947fc8850c7a59
malte <109072169+maltemaehler@users.noreply.github.com>
2025-04-22 21:21:21 +0200

    Insert Maven Spring Project Build

---
+++ src/main/java/malte/dev/app/GhostNet/GhostNetApplication.java
@@ -0,0 +1,13 @@
+package malte.dev.app.GhostNet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhostNetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhostNetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/malte/dev/app/GhostNet/GhostNetApplicationTests.java
@@ -0,0 +1,13 @@
+package malte.dev.app.GhostNet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhostNetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
