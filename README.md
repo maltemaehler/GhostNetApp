0c4900f7bf75e0be643abd880ade7f9e7b577714
malte <109072169+maltemaehler@users.noreply.github.com>
2025-04-23 21:16:17 +0200

    Add service and controller class

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # GhostNetApp
+
+## Spring Initialzr Infos
+- Group: malte.dev.app
+- Artifact: GhostNet
+- Name: GhostNet
+- Package name:
+- Java version: 17
+
+## Dependencies
+- Spring Boot
  
---
+++ src/main/java/malte/dev/app/GhostNet/SimpleOutputService.java
@@ -0,0 +1,10 @@
+package malte.dev.app.GhostNet;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleOutputService {
+    public String getGreeting() {
+        return "Simple Output";
+    }
+}
