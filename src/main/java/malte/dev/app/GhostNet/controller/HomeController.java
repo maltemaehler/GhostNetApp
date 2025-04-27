dc05ac6400e10390075dec6cd8a88a83c6b7542e
malte <109072169+maltemaehler@users.noreply.github.com>
2025-04-27 09:34:46 +0200

    added index.html and respective controller

---
+++ src/main/java/malte/dev/app/GhostNet/controller/HomeController.java
@@ -0,0 +1,13 @@
+package malte.dev.app.GhostNet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "index"; // Looks for 'index.html' in src/main/resources/templates
+    }
+}
